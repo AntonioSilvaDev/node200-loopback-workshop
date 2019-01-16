d60b97a88bd82ddcd5a07a48d0e6229f41aa5b17
TonyS <tonysilvasd@gmail.com>
2019-01-15 20:17:35 -0800

    adds config.yml file for circleCI

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -3,11 +3,6 @@ module.exports = {
         "name": "db",
         "connector": "memory"
     },
-    "mLabs": {
-        "connector": "loopback-connector-mongodb",
-        "url": `mongodb://${process.env.MLAB_USER_NAME}:${process.env.MLAB_PW}@ds157654.mlab.com:57654/loopbacktest`,
-        "name": "mLabs"
-    },
     "MongoDB": {
         "connector": "loopback-connector-mongodb",
         "name": "MongoDB"
